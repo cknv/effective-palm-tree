d71af6ede0127bc37dbc98035a9bb8b77f442139
Esben Sonne <esbensonne@gmail.com>
2017-10-05 16:41:32 +0200

    One more read function to prove the case
    Also converted tabs to spaces.

--- dashboard/api/server.js
+++ dashboard/api/server.js
@@ -8,55 +8,58 @@ const store = require('./store')
 const app = express();
 
 app.post('/', (request, response) => {
-	response.send('received');
-	for (var i = 0; i < connections.length; i++) {
-		pushLatestData(connections[i]);
-	}
+    response.send('received');
+    for (var i = 0; i < connections.length; i++) {
+        pushLatestData(connections[i]);
+    }
 });
 const httpServer = http.createServer(app);
 
 httpServer.listen(1337, function() {});
 
 const wsServer = new WebSocketServer({
-	httpServer: httpServer
+    httpServer: httpServer
 });
 
 connections = [];
 
 function pushLatestData(connection) {
-	pushLatestData2(connection)
-		.then(res => console.log('pushed data.'))
-		.catch(err => setImmediate(() => { throw err}))
+    pushLatestData2(connection)
+        .then(res => console.log('pushed data.'))
+        .catch(err => setImmediate(() => { throw err}))
 
 
-	// uniqueDevices
-	// totalCalls
-	// CAs detected
-	// avgDetectionTime
-	// timeSeries
-	// console.log('pushed data.');
+    // uniqueDevices
+    // totalCalls
+    // CAs detected
+    // avgDetectionTime
+    // timeSeries
+    // console.log('pushed data.');
 }
 
 async function pushLatestData2(connection) {
-	const devices = await store.readUniqueDevices()
-	connection.sendUTF(
-		JSON.stringify({
-			uniqueDevices: parseInt(devices[0].devices),
-		})
-	);
+    const devices = await store.readUniqueDevices()
+    const totalCalls = await store.readTotalCalls()
+
+    connection.sendUTF(
+        JSON.stringify({
+            uniqueDevices: parseInt(devices[0].devices),
+            totalCalls: parseInt(totalCalls[0].calls),
+        })
+    );
 }
 
 wsServer.on('request', function(request) {
-	const connection = request.accept(null, request.origin);
-	console.log((new Date()) + ' Connection from origin ' + request.origin + '.')
+    const connection = request.accept(null, request.origin);
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.')
 
-	const connectionIndex = connections.push(connection) - 1;
-	console.log('Connection accepted, Current connections: ' + connections.length);
+    const connectionIndex = connections.push(connection) - 1;
+    console.log('Connection accepted, Current connections: ' + connections.length);
 
-	connection.on('close', function(connection) {
-		connections.splice(connectionIndex, 1);
-		console.log('Connection closed, Remaining connections: ' + connections.length);
-	});
+    connection.on('close', function(connection) {
+        connections.splice(connectionIndex, 1);
+        console.log('Connection closed, Remaining connections: ' + connections.length);
+    });
 
-	pushLatestData(connection);
+    pushLatestData(connection);
 })
--- dashboard/api/store/index.js
+++ dashboard/api/store/index.js
@@ -1,27 +1,32 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-	user: 'dashboard',
-	host: 'localhost',
-	database: 'dashboard_db',
-	port: 5435,
+    user: 'dashboard',
+    host: 'localhost',
+    database: 'dashboard_db',
+    port: 5435,
 })
 
 async function readFromDb(sql) {
-	const result = await pool.query(sql)
-	return result.rows
+    const result = await pool.query(sql)
+    return result.rows
 }
 
 async function readUniqueDevices() {
-	var rows = await readFromDb("SELECT COUNT(DISTINCT deviceId) AS devices FROM calls;")
-	return rows
+    var rows = await readFromDb("SELECT COUNT(DISTINCT deviceId) AS devices FROM calls;")
+    return rows
+}
+
+async function readTotalCalls() {
+    const rows = await readFromDb("SELECT COUNT(*) AS calls FROM calls;")
+    return rows
 }
 
 module.exports = {
-	// query: (text, params, callback) => {
-	// 	return pool.query(text, params, callback)
-	// }
-	readUniqueDevices: () => {
-		return readUniqueDevices()
-	},
+    readUniqueDevices: () => {
+        return readUniqueDevices()
+    },
+    readTotalCalls: () => {
+        return readTotalCalls()
+    }
 }

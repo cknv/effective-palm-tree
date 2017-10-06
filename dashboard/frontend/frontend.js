5b10ca82c93422defe6052b873d2762fbb4bcb4a
Esben Sonne <esbensonne@gmail.com>
2017-10-06 08:12:56 +0200

    Got a hardcoded chart working
    Also got a few stats in place, nothing big.

--- dashboard/frontend/frontend.js
+++ dashboard/frontend/frontend.js
@@ -1,23 +1,37 @@
 var app = new Vue({
-	el: '#app',
-	data: {
-		message: 'Hello Vue!',
-		devices: null,
-		totalCalls: null,
-	}
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    devices: null,
+    totalCalls: null,
+    avgDetectionTime: null,
+  }
 })
 
 var connection = new WebSocket('ws://localhost:1337');
 
-connection.onmessage = function (message) {
-	try {
-		var parsed = JSON.parse(message.data)
-	} catch (e) {
-		console.log('Received invalid json: ' + message.data)
-		return
-	}
-	console.log(parsed)
+function renderChart() {
+  const data = {
+    // labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    series: [
+      [5, 2, 4, 2, 0.5, 4, 5, 6, 2, 2, 2, 2, 2]
+    ],
+  }
+  new Chartist.Bar('.ct-chart', data);
+}
 
-	app.devices = parsed.uniqueDevices
-	app.totalCalls = parsed.totalCalls
+connection.onmessage = function (message) {
+  try {
+    var parsed = JSON.parse(message.data)
+  } catch (e) {
+    console.log('Received invalid json: ' + message.data)
+    return
+  }
+  console.log(parsed)
+
+  app.devices = parsed.uniqueDevices
+  app.totalCalls = parsed.totalCalls
+  app.avgDetectionTime = parsed.avgDetectionTime
+
+  renderChart()
 }
